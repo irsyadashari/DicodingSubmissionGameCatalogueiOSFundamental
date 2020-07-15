0aebbcef259beeef97e40c863c8f4ec54cd5d5c5
Irsyad Ashari <irsyad.ashari17@gmail.com>
2020-07-15 21:49:18 +0800

    Finishing home page

--- Game Catalogue Irsyad/Controller/ViewController.swift
+++ Game Catalogue Irsyad/Controller/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet var gameTableView: UITableView!
     var gamesData : [GameData] = []
@@ -16,38 +16,65 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     var gameManager = GameManager()
     
     override func viewDidLoad() {
-           super.viewDidLoad()
-         
-           gameManager.delegate = self
-           gameManager.fetchGames()
-           
-           gameTableView.register(UINib(nibName: "GameTVCell", bundle: nil), forCellReuseIdentifier: "GameCell")
-           gameTableView.delegate = self
-           gameTableView.dataSource = self
-           
-       }
+        super.viewDidLoad()
+        
+        gameManager.delegate = self
+        gameManager.fetchGames()
+        
+        gameTableView.register(UINib(nibName: "GameTVCell", bundle: nil), forCellReuseIdentifier: "GameCell")
+        //           gameTableView.delegate = self
+        gameTableView.dataSource = self
+        
+    }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return gamesData.count
-      }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-          let cell = tableView.dequeueReusableCell(withIdentifier: "GameTVCell", for : indexPath) as! GameTVCell
-          
-          let game = gamesData[indexPath.row]
-          cell.gamePoster.image = UIImage(contentsOfFile: game.gamePoster)
-          cell.gameTitle.text = game.gameTitle
-          cell.gameRating.text = String(game.gameRating)
-          print("table telah di inflate")
-          return cell
-      }
     
-
-
 }
 
+extension ViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gamesData.count
+    }
+    
+    // Set the spacing between sections
+    private func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 5
+    }
+    
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GameCell", for : indexPath) as! GameTVCell
+        
+        let game = gamesData[indexPath.row]
+        
+        let urlPoster = URL(string: game.gamePoster)!
+        
+        getData(from: urlPoster) { data, response, error in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? urlPoster.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() {
+                cell.gamePoster.layer.cornerRadius = 16
+                cell.gamePoster.image = UIImage(data: data)
+            }
+        }
+        
+        cell.gameTitle.text = game.gameTitle
+        cell.gameRating.text = String(game.gameRating)
+        
+        cell.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 16
+        print("table telah di inflate")
+        return cell
+    }
+    
+}
 extension ViewController : GameManagerDelegate{
-   
+    
     func didUpdateGame(_ gameManager: GameManager, game: GamesModel) {
         
         print(game.count)
@@ -55,13 +82,20 @@ extension ViewController : GameManagerDelegate{
         for item in game.results{
             gamesData.append(item)
         }
-        print("Data telah berhasil dimuat")
+        print(gamesData)
+        
+        DispatchQueue.main.async {
+            self.gameTableView.reloadData()
+        }
+        
     }
     
     func didFailWithError(error: Error) {
         print(error)
     }
 }
+
+
 
 
 
