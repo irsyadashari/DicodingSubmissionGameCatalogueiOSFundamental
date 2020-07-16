ba4b45024a82bf3be14a2cd35a00e2f48c6623bf
Irsyad Ashari <irsyad.ashari17@gmail.com>
2020-07-17 07:28:00 +0800

    terubah ketika di scroll

--- Game Catalogue Irsyad/Controller/ViewController.swift
+++ Game Catalogue Irsyad/Controller/ViewController.swift
@@ -7,12 +7,39 @@
 
 import UIKit
 
+enum DownloadState {
+    case new, downloaded, failed
+}
+
+
+
+class Game {
+    let id : Int
+    let title : String
+    let rating : Float
+    var poster : UIImage
+    let releaseDate : String
+    
+    var image: UIImage?
+    var state: DownloadState = .new
+    
+    init(id: Int,title: String, poster: UIImage, rating : Float, releaseDate : String) {
+        self.id = id
+        self.title = title
+        self.poster = poster
+        self.rating = rating
+        self.releaseDate = releaseDate
+    }
+    
+}
+
+var gamesData : [GameData] = []
+var games: [Game] = []
+
+
 class ViewController: UIViewController {
     
     @IBOutlet var gameTableView: UITableView!
-    var gamesData : [GameData] = []
-    
-    var gamePosterDownloaded : [UIImage] = []
     
     var gameManager = GameManager()
     
@@ -27,9 +54,11 @@ class ViewController: UIViewController {
         gameTableView.register(UINib(nibName: "GameTVCell", bundle: nil), forCellReuseIdentifier: "GameCell")
         
     }
+    
 }
 
-extension ViewController : UITableViewDataSource{
+extension ViewController : UITableViewDataSource, UITableViewDelegate{
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return gamesData.count
     }
@@ -44,43 +73,47 @@ extension ViewController : UITableViewDataSource{
         
         let game = gamesData[indexPath.row]
         
-        let urlPoster = URL(string: game.gamePoster)!
+        let urlPoster = URL(string : game.gamePoster)
         
-        cell.gameTitle.text = game.gameTitle
-        cell.gameRating.text = String(game.gameRating)
         
-        //styling
-        cell.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        cell.layer.borderWidth = 1
-        cell.layer.cornerRadius = 16
-        cell.gamePoster.layer.cornerRadius = 16
-        
-        getData(from: urlPoster) { data, response, error in
+        getData(from: urlPoster!) { data, response, error in
             guard let data = data, error == nil else { return }
             
             DispatchQueue.main.async() {
                 //Inflating data at Homepage
+                cell.gameTitle.text = game.gameTitle
+                cell.gameRating.text = String(game.gameRating)
                 cell.gamePoster.image = UIImage(data: data)
+                //styling
+                cell.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                cell.layer.borderWidth = 1
+                cell.layer.cornerRadius = 16
+                cell.gamePoster.layer.cornerRadius = 16
+                
+                if(game.gameId  == games[indexPath.row].id){
+                    games[indexPath.row].poster = UIImage(data: data)!
+                }
             }
         }
         return cell
     }
-}
-
-extension ViewController: UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         // Memanggil View Controller dengan berkas NIB/XIB di dalamnya
         let detail = DetailGameViewController(nibName: "DetailGameViewController", bundle: nil)
-        
-        // Mengirim data hero
-//        detail.game = gamesData[(indexPath.row)]
-
+            
+        detail.game = GameModel(
+                           id: games[indexPath.row].id,
+                           poster: games[indexPath.row].poster,
+                           title: games[indexPath.row].title,
+                           releasedDate: games[indexPath.row].releaseDate,
+                           rating: String(games[indexPath.row].rating))
+     
         // Push/mendorong view controller lain
         self.navigationController?.pushViewController(detail, animated: true)
-     }
- }
+    }
+}
 
 extension ViewController : GameManagerDelegate{
     
@@ -88,11 +121,18 @@ extension ViewController : GameManagerDelegate{
         
         for item in game.results{
             gamesData.append(item)
+            
+            games.append(Game(
+                id: item.gameId,
+                title: item.gameTitle,
+                poster: UIImage(imageLiteralResourceName: "loading-image-cardview-game-poster"),
+                rating : item.gameRating,
+                releaseDate : item.gameReleasedDate
+            ))
         }
         
         DispatchQueue.main.async {
             self.gameTableView.reloadData()
-            print("data di reload")
         }
         
     }
